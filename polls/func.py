f720abf3036e5756d10ae082a1ad91013d193aed
Arseniy Kocharov <mail.dev012@gmail.com>
2021-05-25 12:54:11 +0300

    Забыл инициализировать раньше. Реализовал АТБАШ и Белазо'

---
+++ polls/func.py
@@ -0,0 +1,65 @@
+def atbash(message, flag):
+    alphavite = ',.!:\'\"#?@[](){} '
+    if flag == 'encrypt':
+        text = message
+        code = ''  # Зашифрованное сообщение
+        for i in text:  # блок шифрования
+            if i.isupper():
+                k = ord(i) % ord('А')  # находим позицию символа в алфавите (начиная с 0)
+                code += chr(ord('Я') - k)  # выбираем из алфавита символ, который меньше на k+1 чем длина алфавита
+            elif i.islower():
+                k = ord(i) % ord('а')
+                code += chr(ord('я') - k)
+            else:
+                code += alphavite[len(alphavite) - alphavite.find(i) - 1]
+        return code  # Выводим зашифрованное сообщение
+    else:
+        text_decode = message
+        global decode
+        decode = ''
+        for i in text_decode:  # блок расшифрования
+            if i.isupper():
+                k = ord(i) % ord('А')  # находим позицию символа в алфавите (начиная с 0)
+                decode += chr(ord('Я') - k)  # выбираем из алфавита сивол, который меньше на k+1 чем длина алфавита
+            elif i.islower():
+                k = ord(i) % ord('а')
+                decode += chr(ord('я') - k)
+            else:
+                decode += alphavite[len(alphavite) - alphavite.find(i) - 1]
+        return decode
+
+def belazo(message,flg,user_key):
+    alphavite = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'  # алфавит
+    alphavite2 = alphavite.upper()
+    alphavite3 = ',.!:\'\"#?@[](){} '
+    if flg == 'encrypt':
+        text = message  # Ввод сообщения
+        code = ''  # Зашифрованное сообщение
+        key = user_key
+        krya = 0
+
+        for i in text:  # блок шифрования
+            if i.islower():
+                code += alphavite[(alphavite.find(i) + alphavite.find(key[krya]) + 1) % len(alphavite)]
+            elif i.isupper():
+                code += alphavite2[(alphavite2.find(i) + alphavite2.find(key[krya].upper()) + 1) % len(alphavite2)]
+            else:
+                code += alphavite3[(alphavite3.find(i) + alphavite3.find(key[krya]) - 1) % len(alphavite3)]
+            krya = (krya + 1) % len(key)
+        return code  # вывод зашифровнного текста
+    else:
+        decode = ''
+        krya = 0
+        code = message
+        key = user_key
+        for i in code:
+            if i.islower():
+                decode += alphavite[(alphavite.find(i) - alphavite.find(key[krya]) - 1) % len(alphavite)]
+            elif i.isupper():
+                decode += alphavite2[(alphavite2.find(i) - alphavite2.find(key[krya].upper()) - 1) % len(alphavite2)]
+            else:
+                decode += alphavite3[(alphavite3.find(i) - alphavite3.find(key[krya]) + 1) % len(alphavite3)]
+            krya = (krya + 1) % len(key)
+
+        return decode
+
---
+++ polls/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+from django.http import HttpResponse , Http404 , HttpResponseRedirect
+import re
+import polls.func as f
+
+
+def index(request):
+    if request.method == 'POST':
+        warning_list = list()
+        message = request.POST['message']
+        key = request.POST['key']
+        flag_crypt = int(request.POST['flag_crypt'])
+        flag_action = request.POST['flag_action']
+        response = str()
+
+        if flag_crypt in [1] and key != '': # проверка на передачу ключа в те шифры, где это не нужно
+            warning_list.append('Для данного шифра ключ не нужен. Игнорируем ключ')
+
+        if flag_crypt == 1:
+            response = f.atbash(message,flag_action)
+        elif flag_crypt == 2:
+            response = f.belazo(message,flag_action,key)
+        elif flag_crypt == 3:
+            pass
+        elif flag_crypt == 4:
+            pass
+        elif flag_crypt == 5:
+            pass
+        elif flag_crypt == 6:
+            pass
+        elif flag_crypt == 7:
+            pass
+        elif flag_crypt == 8:
+            pass
+        elif flag_crypt == 9:
+            pass
+        elif flag_crypt == 10:
+            pass
+        elif flag_crypt == 11:
+            pass
+        elif flag_crypt == 12:
+            pass
+        elif flag_crypt == 13:
+            pass
+        elif flag_crypt == 14:
+            pass
+        elif flag_crypt == 15:
+            pass
+        elif flag_crypt == 16:
+            pass
+        else:
+            warning_list.append('Неправильно выбран шифр')
+        context = {
+            'result': response,
+            'warning_list' : warning_list
+        }
+        return render(request,'index.html',context= context)
+    else:
+        return render(request,'index.html')
